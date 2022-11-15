c6bf8a8475693b6bcfd9c95aff0c63a2b0a56cef
Ahmaddjanuardi <ahmaddjanuardi@gmail.com>
2022-11-15 10:46:17 +0700

    latihanJS

---
+++ latihanjs/latihan.js
@@ -0,0 +1,20 @@
+// const kucing ={
+//     nama: "mamat",
+//     warna: "Kuning",
+//     jumlahKaki: 4
+// }
+// console.log(kucing.nama)
+
+// const response = {
+//     statusCode: 200,
+//     endpoint: 'GET /api/v1/dummies',
+//     body: {
+//       message: 'Hello World',
+//       data: [
+//         { id: 1, name: 'Andre', age: 20 },
+//         { id: 2, name: 'Bella', age: 21 },
+//         { ... },
+//      ],
+//     totalData: 100,
+//     responseTime: '1506ms',
+//   };
